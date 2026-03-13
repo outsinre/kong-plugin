c8e61728e02fb3ac1a891c48336a1dee12011bbd
Zachary Hu <zachary.hu@konghq.com>
2026-03-13 15:23:44 +0800

    fix(version): plugin version

--- kong/plugins/myplugin/handler.lua
+++ kong/plugins/myplugin/handler.lua
@@ -11,10 +11,12 @@
 ---------------------------------------------------------------------------------------------
 
 
+local kong_meta = require "kong.meta"
+
 
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
-  VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
+  VERSION = kong_meta.core_version, -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
 
 
